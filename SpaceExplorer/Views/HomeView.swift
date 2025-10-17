08a3a960b66dae322bfeba4c968e8618be2ca1a9
Bkhamilton <bkhamilton310@gmail.com>
2025-10-16 21:58:28 -0400

    Refactor HomeView to use HomePictureSection for displaying space pictures

--- SpaceExplorer/Views/HomeView.swift
+++ SpaceExplorer/Views/HomeView.swift
@@ -28,28 +28,7 @@ struct HomeView: View {
                 }
                 
                 List(_pictures) { picture in
-                    VStack(alignment: .leading, spacing: 8) {
-                        HStack {
-                            Image(systemName: picture.imageName)
-                                .font(.largeTitle)
-                                .foregroundColor(.blue)
-                                .frame(width: 60, height: 60)
-                            
-                            VStack(alignment: .leading) {
-                                Text(picture.title)
-                                    .font(.headline)
-                                Text(picture.date)
-                                    .font(.caption)
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        
-                        Text(picture.description)
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .lineLimit(3)
-                    }
-                    .padding(.vertical, 8)
+                    HomePictureSection(picture: picture)
                 }
                 .navigationTitle("Space Pictures")
                 .toolbar {
